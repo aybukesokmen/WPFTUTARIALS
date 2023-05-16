80551b284f90ba338aeb58f4bf9693aa1e487f7d
asokmen <aybukesokmen@gmail.com>
2023-05-16 10:01:14 +0300

    first commit

---
+++ page/PageDeneme/NavigationStore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+
+namespace PageDeneme
+{
+    public class NavigationStore
+    {
+        private ViewModelBase _CurrentViewModel;
+        public ViewModelBase CurrentViewModel {
+            get=>_CurrentViewModel; set
+            {
+                _CurrentViewModel = value;
+            }
+        }
+    }
+}
